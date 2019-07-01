88419d89b5ac6dfed11b75d8592c23c252b94e96
Swathi Surapaneni <ssurapaneni@clearent.com>
2019-07-01 17:54:39 -0500

    Initial commit

---
+++ src/Test.java
@@ -0,0 +1,20 @@
+public class Test {
+
+    public static void main (String[] args){
+        int[] numbers= {1,2,3,4,5,6,7,8,9};
+      int count=0;
+      for (count=0; count<numbers.length; count++){
+
+          System.out.println("count is: " + count);
+                      if(numbers[count]==3){
+                          System.out.println("hello world");
+                      }else{
+                          System.out.println("carl is an expert");
+                      }
+                  }
+              }
+          }
+
+
+
+
